0f316906f1a982d37bc5272f0104608492fd384a
Devadarshan Pushkaran <91815543+dpushkaran@users.noreply.github.com>
2025-07-27 20:56:01 -0500

    updated due date format

--- get_assignments.py
+++ get_assignments.py
@@ -1,12 +1,15 @@
 from dotenv import load_dotenv
 import os
 import requests
+from datetime import datetime
 
 load_dotenv()
 token = os.getenv("CANVAS_API_TOKEN")
 
 courses_url = "https://canvas.unl.edu/api/v1/users/self/favorites/courses"
 assignments_url = "https://canvas.unl.edu/api/v1/courses/{course_id}/assignments"
+date_format = "%A, %B %-d at %-I:%M %p, %Y"
+
 
 session = requests.Session()
 session.headers.update({"Authorization": f"Bearer {token}"})
@@ -26,6 +29,8 @@ if r.status_code == 200:
             assignments = r.json()
             for assignment in assignments:
                 if assignment['due_at'] != None:
-                    print(f"- {assignment['name']} (ID: {assignment['id']})")
+                    due_date = datetime.fromisoformat(assignment['due_at'])
+                    print(f"- {assignment['name']} (ID: {assignment['id']}) due at {due_date.strftime(date_format)}")
                 
                 #print(f"- {assignment['name']} (ID: {assignment['id']})")
+
